7c48c2d93f9a89978032896565df8a00e9bdce6c
Davi Morais <davi.a.morais@aluno.senai.br>
2026-04-16 17:11:19 -0300

    MOD: Finalizado o sistema de cadastro e login.

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request
 from model.produto import recuperar_produtos
+from model.usuario import cadastrar, login
 
 app = Flask(__name__)
 
@@ -21,8 +22,29 @@ def page_product(id):
 def page_signup():
     return render_template("cadastro.html")
 
+
+@app.route("/cadastro/send", methods=["POST"])
+def cadastro_post():
+    user = request.form.get("nome")
+    senha = request.form.get("senha")
+    
+    if cadastrar(user, senha):
+        return redirect("/")
+    else:
+        return redirect("/cadastro")
+    
 @app.route("/login")
 def page_login():
     return render_template("login.html")
+
+@app.route("/login/send", methods=["POST"])
+def login_post():
+    user = request.form.get("nome")
+    senha = request.form.get("senha")
+    
+    if login(user, senha):
+        return redirect("/")
+    else:
+        return redirect("/login")
 
 app.run(host='0.0.0.0', port=8080, debug=True)
--- model/usuario.py
+++ model/usuario.py
@@ -1,6 +1,23 @@
 from database.conexao import conectar
 import mysql.connector
 
-def cadastrar(usuario, senha):
+def cadastrar(user, senha):
     conexao, cursor = conectar()
     
+    try:
+        cursor.execute("INSERT INTO usuario(username, senha) VALUES (%s, %s)", (user, senha))
+        conexao.commit()
+        conexao.close()
+        return True
+    except:
+        return False
+    
+def login(user, senha):
+    conexao, cursor = conectar()
+    cursor.execute("SELECT username, senha FROM usuario WHERE username = %s", (user,))
+    resultado = cursor.fetchone()
+
+    if resultado['senha']  == senha:
+        return True
+    
+    return False
